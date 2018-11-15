9685e3850879cf62ebeb4f1d29c60f189845a875
sanaboina prasad <sssrkvp@gmail.com>
2018-11-15 22:36:56 +0530

    Initial Commit

---
+++ SampleTableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleTableview
+//
+//  Created by Sriram Prasad on 15/11/18.
+//  Copyright © 2018 FullStackNet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
